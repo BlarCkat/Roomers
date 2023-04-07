582d2dcc6110644042dc6ca51204518e5ec501fe
John St. Aïmond Banson <articwolf177@gmail.com>
2023-04-06 22:20:33 -0700

    starting point

---
+++ components/container.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface ContainerProps {
+    children: React.ReactNode
+}
+
+const Container = () => {
+    return ( 
+        <>
+        <div className="container">
+
+        </div>
+        </>
+     );
+}
+ 
+export default Container;
---
+++ pages/layout.tsx
@@ -0,0 +1,22 @@
+import NavBarComponent from "../components/navbar";
+
+export const metadata = {
+    title: 'Roomers',
+    description: 'Room renting and booking platform'
+}
+
+const LayoutView = ({children}:{children: React.ReactNode}) => {
+
+    return ( 
+        <>
+        <html>
+        <body>
+        <NavBarComponent/>
+        {children}
+        </body>
+        </html>
+        </>
+     );
+}
+ 
+export default LayoutView;
